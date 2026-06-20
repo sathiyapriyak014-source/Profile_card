e5a2d176017bd8275dbc5579262dfd828bb34726
SATHIYA PRIYA K <sathiyapriyak014@gmail.com>
2026-06-20 01:11:43 -0700

    Add files via upload

---
+++ app.py
@@ -0,0 +1,28 @@
+from flask import Flask, render_template, request
+
+app = Flask(__name__)
+
+# Home page
+@app.route("/")
+def home():
+    return render_template("index.html")
+
+
+# Display profile card
+@app.route("/profile", methods=["POST"])
+def profile():
+
+    name = request.form["name"]
+    bio = request.form["bio"]
+    image = request.form["image"]
+
+    return render_template(
+        "profile.html",
+        name=name,
+        bio=bio,
+        image=image
+    )
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
